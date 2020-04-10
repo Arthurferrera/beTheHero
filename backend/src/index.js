6c61c12b30d06abcfd81855c36909a02c54770a2
Arthurferrera <arthur_f.oliveira@hotmail.com>
2020-04-10 05:10:52 -0300

    count itens / cors / join

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,12 @@
 // IMPORTS
 const express = require('express');
+const cors = require('cors');
 const routes = require('./routes');
 
 // CRIANDO A APLICAÇÃO
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
