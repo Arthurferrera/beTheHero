9e51a49956b585d95ec8b9cee12fb96544abd696
Arthurferrera <arthur_f.oliveira@hotmail.com>
2020-04-12 04:06:05 -0300

    integration login -> api

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,6 +1,8 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import { FiLogIn } from 'react-icons/fi'
+
+import api from '../../services/api';
 
 import './styles.css';
 
@@ -8,15 +10,38 @@ import logoIMG from '../../assets/logo.svg'
 import heroesIMG from '../../assets/heroes.png'
 
 export default function Logon() {
+  const [id, setId] = useState('');
+  const history = useHistory();
+
+  async function handleLogin(e) {
+    e.preventDefault();
+
+    try {
+      const response = await api.post('sessions', { id });
+
+      localStorage.setItem('beTheHero.ong', JSON.stringify({id: id, name: response.data.name}));
+
+      history.push('/profile');
+    } catch (error) {
+      alert('Falha no login, tente novamente');
+    }
+  }
+  
   return (
     <div className="logon-container">
       <section className="form">
         <img src={logoIMG} alt="Be The Hero"/>
 
-        <form>
+        <form onSubmit={handleLogin}>
           <h1>Faça seu logon</h1>
 
-          <input type="text" name="" id="" placeholder="Sua Id"/>
+          <input
+            value={id}
+            onChange={e => setId(e.target.value)}
+            type="text"
+            name="idOng"
+            id="idOng"
+            placeholder="Sua Id"/>
           <button className="button" type="submit">Entrar</button>
           <Link className="back-link" to="/register">
             <FiLogIn size={16} color="#E02041" />
--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { FiPower, FiTrash2 } from 'react-icons/fi';
 
@@ -7,11 +7,14 @@ import logoIMG from '../../assets/logo.svg'
 import './styles.css';
 
 export default function Profile() {
+  const ongName = JSON.parse(localStorage.getItem('beTheHero.ong')).name;
+  
+  
   return (
     <div className="profile-container">
       <header>
         <img src={logoIMG} alt="Be The Hero"/>
-        <span>Bem vinda, ONG</span>
+        <span>Bem vinda, {ongName}</span>
 
         <Link className="button" to="/incidents/new">
           Cadastrar novo caso
